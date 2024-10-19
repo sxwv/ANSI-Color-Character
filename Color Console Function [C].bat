cee10c0e5744b9754757f52772e242faae83305f
sxw <omgnotbot1@gmail.com>
2024-10-19 14:37:03 -0700

    Add files via upload

---
+++ Color Console Function [C].bat
@@ -0,0 +1,58 @@
+REM ====================
+REM    Color Console
+REM ====================
+
+:c
+
+:colorPrint Color  Str  [/n]
+setlocal
+set "s=%~2"
+call :colorPrintVar %1 s %3
+exit /b
+
+:colorPrintVar  Color  StrVar  [/n]
+if not defined DEL call :initColorPrint
+setlocal enableDelayedExpansion
+pushd .
+':
+cd \
+set "s=!%~2!"
+:: The single blank line within the following IN() clause is critical - DO NOT REMOVE
+for %%n in (^"^
+
+^") do (
+  set "s=!s:\=%%~n\%%~n!"
+  set "s=!s:/=%%~n/%%~n!"
+  set "s=!s::=%%~n:%%~n!"
+)
+for /f delims^=^ eol^= %%s in ("!s!") do (
+  if "!" equ "" setlocal disableDelayedExpansion
+  if %%s==\ (
+    findstr /a:%~1 "." "\'" nul
+    <nul set /p "=%DEL%%DEL%%DEL%"
+  ) else if %%s==/ (
+    findstr /a:%~1 "." "/.\'" nul
+    <nul set /p "=%DEL%%DEL%%DEL%%DEL%%DEL%"
+  ) else (
+    >colorPrint.txt (echo %%s\..\')
+    findstr /a:%~1 /f:colorPrint.txt "."
+    <nul set /p "=%DEL%%DEL%%DEL%%DEL%%DEL%%DEL%%DEL%"
+  )
+)
+if /i "%~3"=="/n" echo(
+popd
+exit /b
+
+
+:initColorPrint
+for /f %%A in ('"prompt $H&for %%B in (1) do rem"') do set "DEL=%%A %%A"
+<nul >"%temp%\'" set /p "=."
+subst ': "%temp%" >nul
+exit /b
+
+
+:cleanupColorPrint
+2>nul del "%temp%\'"
+2>nul del "%temp%\colorPrint.txt"
+>nul subst ': /d
+exit /b
